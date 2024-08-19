d2d46669139e0f3d7823f7f6e1d2f3626b500f3d
haiiliin <haiiliin@users.noreply.github.com>
2024-08-19 17:32:15 +0000

    deploy: b18e783a2dfbf44c0c0e496478a742034a4397b1

--- en/2019/_static/documentation_options.js
+++ en/2019/_static/documentation_options.js
@@ -1,7 +1,6 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2019.5.7',
-    LANGUAGE: 'en/html',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2019.7.6',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
