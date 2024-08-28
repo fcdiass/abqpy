82015ede5f18302a1146fb51b732f9b985c6ca09
haiiliin <haiiliin@users.noreply.github.com>
2024-08-28 08:44:05 +0000

    deploy: 4e9388edd251ac4ddf07631835a53577b7f00303

--- en/dev/_static/documentation_options.js
+++ en/dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2024.7.8.dev1+gdc1d11743',
+    VERSION: '2024.7.8.dev2+g4e9388edd',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
